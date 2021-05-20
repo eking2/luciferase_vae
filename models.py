77e931ebb7b825987395674f171a8279c12d8b90
eking2 <eking2@uci.edu>
2021-05-20 00:48:21 -0700

    finish msavae model

--- models.py
+++ models.py
@@ -11,6 +11,9 @@ class fc_encoder(nn.Module):
 
         super().__init__()
 
+        self.seq_len = seq_len
+        self.alphabet_size = alphabet_size
+
         layers = []
 
         # conditional net has concatenated one hot input
@@ -43,12 +46,16 @@ class fc_encoder(nn.Module):
 
     def forward(self, x):
 
+        # flatten if (batch, charset, seq_len)
+        if (x.shape[1] * x.shape[2]) == (self.seq_len * self.alphabet_size):
+            x = x.view(x.shape[0], -1)
+
         x = self.net(x)
 
-        z_mean = self.z_mean(x)
-        z_var = self.z_var(x)
+        mu = self.z_mean(x)
+        logvar = self.z_var(x)
 
-        return z_mean, z_var
+        return mu, logvar
 
 
 class fc_decoder(nn.Module):
@@ -58,7 +65,12 @@ class fc_decoder(nn.Module):
 
         super().__init__()
 
+        self.seq_len = seq_len
+        self.alphabet_size = alphabet_size
+
         layers = []
+
+        act = getattr(nn, activation)
 
         input_dim = latent_dim
         if n_conditions > 0:
@@ -99,8 +111,34 @@ class cnn_encoder(nn.Module):
 
 class MSA_VAE(nn.Module):
 
-    def __init__(self):
-        pass
+    def __init__(self, encoder, decoder):
+
+        super().__init__()
+        
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def reparameterize(self, mu, logvar):
+
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+
+        return mu + eps*std
+
+    def forward(self, x):
+
+        mu, logvar = self.encoder(x)
+        z = self.reparameterize(mu, logvar)
+        x_rec = self.decoder(z)
+
+        return x_rec, mu, logvar
+
+    def vae_loss(self, x, x_rec, mu, logvar):
+
+        recon_loss = F.cross_entropy(x_rec, x, reduction='sum')
+        kl_loss = -0.5 * torch.sum(1 + logvar - mu**2 - torch.exp(logvar))
+
+        return recon_loss + kl_loss
 
 
 class AR_VAE(nn.Module):
@@ -111,5 +149,18 @@ class AR_VAE(nn.Module):
 
 if __name__ == '__main__':
 
-    mod = fc_encoder(360, 10)
-    print(mod)
+    enc_input = torch.randn(2, 21, 360)
+    encoder = fc_encoder(360, 10)
+    mu, logvar = encoder(enc_input)
+    print(mu.shape)
+    print(logvar.shape)
+
+    dec_input = torch.randn(2, 10)
+    decoder = fc_decoder(360, 10)
+    x_rec = decoder(dec_input)
+    print(x_rec.shape)
+
+    vae = MSA_VAE(encoder, decoder)
+    x_rec, mu, logvar = vae(enc_input)
+    print(x_rec.shape)
+
